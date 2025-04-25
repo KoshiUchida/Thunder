7c7ec217e754afb86b045844780c772f1cebb4b9
KoshiUchida <it242047@trident.ac.jp>
2025-04-25 21:59:17 +0900

    PDCAoAI3

--- main.cpp
+++ main.cpp
@@ -10,8 +10,48 @@ constexpr int MapSize = 50;
 constexpr int Rage = 2;
 constexpr int SubLife = 5;
 
+template <typename T>
+inline T clamp(const T& val, const T& low, const T& high) {
+    return std::max(low, std::min(val, high));
+}
+
 struct Position {
     int x, y;
+};
+
+class Map2D {
+public:
+    Map2D(int size) : size(size), map(size, vector<int>(size, 0)) {}
+
+    void clear() {
+        for (auto& row : map)
+            fill(row.begin(), row.end(), 0);
+    }
+
+    void mark(int x, int y) {
+        if (inBounds(x, y))
+            map[y][x] = 1;
+    }
+
+    void draw() const {
+        cout << string(size * 2, '-') << '\n';
+        for (const auto& row : map) {
+            for (int cell : row)
+                cout << (cell ? "口" : "　");
+            cout << '\n';
+        }
+        cout << string(size * 2, '-') << '\n';
+    }
+
+    int getSize() const { return size; }
+
+private:
+    int size;
+    vector<vector<int>> map;
+
+    bool inBounds(int x, int y) const {
+        return x >= 0 && x < size && y >= 0 && y < size;
+    }
 };
 
 struct Mouse {
@@ -19,43 +59,88 @@ struct Mouse {
     Position rota;
     int life;
 
-    inline void step() {
+    void step() {
         pos.x += rota.x;
         pos.y += rota.y;
         life--;
     }
 
-    [[nodiscard]]
-    inline bool isAlive() const {
+    bool isAlive() const {
         return life > 0;
     }
 };
 
-// 型定義で可読性強化
-using Map = vector<vector<int>>;
+class ThunderSimulator {
+public:
+    ThunderSimulator(Map2D& map, mt19937& rng)
+        : map(map), rng(rng), mainPos{ map.getSize() / 2, 0 }, mainDir{ 0, 1 } {}
 
-void draw(const Map& map);
-void thunder(Map& map, mt19937& rng);
+    void simulate() {
+        map.mark(mainPos.x, mainPos.y);
 
-inline int randInt(mt19937& rng, int min, int max) {
-    uniform_int_distribution<> dist(min, max);
-    return dist(rng);
-}
+        while (mainPos.y < map.getSize() - 1) {
+            advanceMain();
+            updateBranches();
+        }
+    }
+
+private:
+    Map2D& map;
+    mt19937& rng;
+    Position mainPos;
+    Position mainDir;
+    vector<Mouse> branches;
+
+    int randInt(int min, int max) {
+        uniform_int_distribution<> dist(min, max);
+        return dist(rng);
+    }
+
+    void advanceMain() {
+        mainPos.x += mainDir.x;
+        mainPos.y += mainDir.y;
+
+        map.mark(mainPos.x, mainPos.y);
+        map.mark(mainPos.x - 1, mainPos.y);
+        map.mark(mainPos.x + 1, mainPos.y);
+
+        // ランダム変化
+        if (randInt(0, Rage - 1) == 0) {
+            mainDir.x += (randInt(0, 1) == 0) ? 1 : -1;
+            mainDir.x = clamp(mainDir.x, -1, 1);
+
+            if (randInt(0, 1) == 0) {
+                int branchX = clamp(mainDir.x + ((randInt(0, 2) == 0) ? 1 : -1), -1, 1);
+                branches.push_back(Mouse{ mainPos, { branchX, 1 }, SubLife });
+            }
+        }
+    }
+
+    void updateBranches() {
+        for (auto& m : branches) {
+            if (m.isAlive()) {
+                m.step();
+                map.mark(m.pos.x, m.pos.y);
+            }
+        }
+
+        branches.erase(remove_if(branches.begin(), branches.end(),
+            [](const Mouse& m) { return !m.isAlive(); }),
+            branches.end());
+    }
+};
 
 int main() {
     random_device rd;
     mt19937 rng(rd());
 
-    Map map(MapSize, vector<int>(MapSize));
+    Map2D map(MapSize);
 
     while (true) {
-        for (auto& row : map)
-            fill(row.begin(), row.end(), 0);
-
-        map[0][MapSize / 2] = 1;
-
-        thunder(map, rng);
-        draw(map);
+        map.clear();
+        ThunderSimulator thunder(map, rng);
+        thunder.simulate();
+        map.draw();
 
         string input;
         cout << "[Enter] で次 / n で終了 > ";
@@ -64,64 +149,4 @@ int main() {
     }
 
     return 0;
-}
-
-void draw(const Map& map) {
-    cout << string(MapSize * 2, '-') << endl;
-    for (const auto& row : map) {
-        for (int cell : row)
-            cout << (cell ? "口" : "　");
-        cout << '\n';
-    }
-    cout << string(MapSize * 2, '-') << endl;
-}
-
-template <typename T>
-inline T clamp(const T& val, const T& low, const T& high) {
-    return std::max(low, std::min(val, high));
-}
-
-
-void thunder(Map& map, mt19937& rng) {
-    Position mainMouse{ MapSize / 2, 0 };
-    Position rotate{ 0, 1 };
-
-    vector<Mouse> mouses;
-
-    while (mainMouse.y < MapSize - 1) {
-        mainMouse.x += rotate.x;
-        mainMouse.y += rotate.y;
-
-        if (mainMouse.x >= 0 && mainMouse.x < MapSize) {
-            if (mainMouse.x > 0)
-                map[mainMouse.y][mainMouse.x - 1] = 1;
-            map[mainMouse.y][mainMouse.x] = 1;
-            if (mainMouse.x < MapSize - 1)
-                map[mainMouse.y][mainMouse.x + 1] = 1;
-        }
-
-        // ランダムな方向変化と分岐
-        if (randInt(rng, 0, Rage - 1) == 0) {
-            rotate.x += (randInt(rng, 0, 1) == 0) ? 1 : -1;
-            rotate.x = clamp(rotate.x, -1, 1);
-
-            if (randInt(rng, 0, 1) == 0) {
-                int branchX = rotate.x + ((randInt(rng, 0, 2) == 0) ? 1 : -1);
-                branchX = clamp(branchX, -1, 1);
-                mouses.emplace_back(Mouse{ mainMouse, {branchX, 1}, SubLife });
-            }
-        }
-
-        for (auto& m : mouses) {
-            if (m.isAlive()) {
-                m.step();
-                if (m.pos.x >= 0 && m.pos.x < MapSize && m.pos.y < MapSize)
-                    map[m.pos.y][m.pos.x] = 1;
-            }
-        }
-
-        mouses.erase(remove_if(mouses.begin(), mouses.end(),
-            [](const Mouse& m) { return !m.isAlive(); }),
-            mouses.end());
-    }
 }
